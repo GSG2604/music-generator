94a2821a13ed78c6bc93ad8263184d7c9a1229dd
GSG <64393510+GSG2604@users.noreply.github.com>
2021-07-16 10:13:01 -0400

    Update and rename cancion.py to example.py

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+#importamos la libreria
 from music import *
 
 s1_n = [Do, Re, ReS, Re, ReS, Sol, Re, Sol, Do, LaS, Do, ReS, LaS, Sol, SolS, ReS, Re, ReS]
@@ -6,18 +7,18 @@ s1_d = [B, B, dot(W), B, W, W, dot(R), W, dot(W), B, W, W, dot(R), W, W, B, W, W
 
 feed = [i for i in zip(s1_n, s1_o, s1_d)]
 
-
+#iniciamos la fuente, 
+#el primer argumento es una tupla que reprecenta el tempo, (figura, BPM)
+#el segundo es una lista de listas, donde cada una tiene 3 valores, la nota, la octava, y la figura (duración)
 s1 = MusicSource((B, 200), feed)
 
 
-
+#iniciamos el reproductor
 player = MusicPlayer()
 
+#agregamos la fuente al reproductor
 player.add_source(s1)
 
-
-
-
-
+#reproduce todas las fuentes en conjunto
 player.play()
 
